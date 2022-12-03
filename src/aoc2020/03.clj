96c2d8b90edfc4d67cc6d2c8b487181bc8ed93c0
Joanne Cheng <chengjoanne2@gmail.com>
2022-12-03 03:42:53 -0500

    day03 2020

--- src/aoc2020/03.clj
+++ src/aoc2020/03.clj
@@ -1,7 +1,42 @@
-(ns aoc2020.03)
+(ns aoc2020.03
+  (:require [clojure.java.io :as io]
+            [clojure.string :as cstr]))
 
+(defn tree?
+  [row pos]
+  (= (nth (cycle row) pos) \#))
 
-(def sample-input
+(defn trees-right-down1 [input right-n]
+  (reduce
+   (fn [coll row]
+     (cond-> (update coll :x + right-n)
+       (tree? row (:x coll)) (update :trees inc)))
+   {:x 0 :trees 0}
+   input))
+
+(defn part2
+  [input]
+  (reduce
+   (fn [coll {:keys [trees]}]
+     (* coll trees))
+   1
+   [(trees-right-down1 input 1)
+    (trees-right-down1 input 3)
+    (trees-right-down1 input 5)
+    (trees-right-down1 input 7)
+    (trees-right-down1
+     (->> (partition 2 2 input)
+          (mapv first))
+     1)]))
+
+;; inputs
+(def real-input
+  (clojure.string/split
+   (slurp
+    (io/resource "aoc2020/day03.txt"))
+   #"\n"))
+
+(def sample-input-text
   "..##.......
 #...#...#..
 .#....#..#.
@@ -14,15 +49,16 @@
 #...##....#
 .#..#...#.#")
 
-(defn trees-right3-down1 [input]
-  (reduce
-   (fn [coll row]
-     (if (= (nth (cycle row) (:x coll)) \#)
-       (-> coll
-           (update :x + 3)
-           (update :trees inc))
-       (update coll :x + 3)))
-   {:x 0 :trees 0}
-   (clojure.string/split input #"\n")))
+(def sample-input
+  (clojure.string/split
+   sample-input-text
+   #"\n"))
 
-(trees-right3-down1 sample-input)
+
+;; part 1
+(trees-right-down1 sample-input 3)
+(trees-right-down1 real-input 1)
+
+;; part 2
+(part2 sample-input)
+(part2 real-input)
